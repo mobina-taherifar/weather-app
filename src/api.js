3f13937b6a981c5766f9f246f84c17f8b3b6febf
Mobina Taherifar <taherifar.dev@gmail.com>
2026-06-23 18:07:20 +0330

    feat: add getWeather function with OpenWeatherMap API

---
+++ src/api.js
@@ -0,0 +1,12 @@
+const API_KEY = "65ae8e88030145e919274e1da3fc3ad7"
+const BASE_URL = "https://api.openweathermap.org/data/2.5"
+
+export async function getWeather(city){
+    try{
+        let response = await fetch(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}`)
+        let data = await response.json();
+        return data;
+    } catch(error){
+        console.log("Something went wrong:", error);
+    }
+}
